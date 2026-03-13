295c8abb578845a8e4a0e9ba60bf1cf44b9e0ae5
San Han <sn1994a@gmail.com>
2026-03-13 14:48:18 +0100

    Read last.ini file for mesh information

--- src/pyramis/dyablo.py
+++ src/pyramis/dyablo.py
@@ -5,11 +5,12 @@ import h5py
 import os
 import numpy as np
 import glob
+import configparser
 
 def check_snapshots(path, prefix=None):
-    timer.start(f"Checking for Dyablo snapshots in {path} with prefix {prefix}...")
     if prefix is None:
         prefix = ANY
+    timer.start(f"Checking for Dyablo snapshots in {path} with prefix {prefix}...")
     pattern = os.path.join(path, config['FILENAME_FORMAT_DYABLO'].format(prefix=prefix, istep=ANY))
     files = glob.glob(pattern)
     scalar_data_dtypes = {}
@@ -41,49 +42,66 @@ def check_snapshots(path, prefix=None):
     timer.record(f"Found {len(snapshots)} Dyablo snapshots in {path} with prefix {prefix}.")
     return snapshots
 
-def read_cell(path, iout=None, istep=None, prefix=None):
-    timer.start(f"Reading Dyablo cell data from {path} with prefix {prefix} and istep {istep}...")
-    if istep is not None:
-        if iout is not None:
-            warnings.warn("Both iout and istep are provided. Ignoring iout and using istep to find the file.")
-        if prefix is None:
-            pattern = os.path.join(path, config['FILENAME_FORMAT_DYABLO'].format(prefix=ANY, istep=istep))
-            files = glob.glob(pattern)
-            if len(files) == 0:
-                raise FileNotFoundError(f"No files found matching pattern {pattern}.")
-            elif len(files) > 1:
-                raise FileExistsError(f"Multiple files found matching pattern {pattern}. Please specify a prefix.")
-            filename = files[0]
-        else:
-            filename = os.path.join(path, config['FILENAME_FORMAT_DYABLO'].format(prefix=prefix, istep=istep))
-    elif iout is not None:
-        if prefix is None:
-            pattern = os.path.join(path, config['FILENAME_FORMAT_DYABLO'].format(prefix=ANY, istep=ANY))
-        else:
-            pattern = os.path.join(path, config['FILENAME_FORMAT_DYABLO'].format(prefix=prefix, istep=ANY))
+
+def read_cell(path, iout=None, istep=None, prefix=None, filename_ini=None):
+    if prefix is None:
+        prefix = ANY
+    if istep is None:
+        istep = ANY
+    if iout is None:
+        iout = ANY
+
+    timer.start(f"Reading Dyablo cell data from {path} with prefix {prefix} and iout {iout} and istep {istep}...")
+    if iout is not ANY or istep is not ANY:
+        pattern = os.path.join(path, config['FILENAME_FORMAT_DYABLO'].format(prefix=prefix, istep=istep))
 
         files = glob.glob(pattern)
+        files.sort()
         if len(files) == 0:
             raise FileNotFoundError(f"No files found matching pattern {pattern}.")
-        files.sort()
-        if iout > 0:
-            iout -= 1
-        elif iout == 0:
-            raise ValueError("iout cannot be 0. It should be a positive or negative integer.")
-        filename = files[iout]
+        if iout is not ANY:
+            if iout > 0:
+                iout -= 1
+            elif iout == 0:
+                raise ValueError("iout cannot be 0. It should be a positive or negative integer.")
+            filename = files[iout]
+        elif len(files) > 1:
+            raise FileExistsError(f"Multiple files found matching pattern {pattern}. Please specify a prefix.")
+        else:
+            filename = files[0]
     else:
-        raise ValueError("Either iout or istep must be provided to find the file.")
+        filename = path
+
+    if filename_ini is None:
+        filename_ini = config['LAST_INI_FILENAME']
+    path_ini = os.path.join(os.path.dirname(filename), filename_ini)
+    if os.path.exists(path_ini):
+        cp = configparser.ConfigParser(inline_comment_prefixes=(';', '#'))
+        cp.read(path_ini)
+        coarse_bins = int(cp['amr']['bx']), int(cp['amr']['by']), int(cp['amr']['bz'])
+        coarse_bins = np.asarray(coarse_bins)
+        domain_extent = np.asarray([float(cp['mesh']['xmax']) - float(cp['mesh']['xmin']),
+                                    float(cp['mesh']['ymax']) - float(cp['mesh']['ymin']),
+                                    float(cp['mesh']['zmax']) - float(cp['mesh']['zmin'])])
+    else:
+        coarse_bins = np.array(config['DEFAULT_COARSE_BINS_DYABLO'])
+        domain_extent = None
+        warnings.warn(f"file {path_ini} not found in the same directory with the snapshot. Using default value of {coarse_bins} for bx, by, bz.")
 
     with h5py.File(filename, 'r') as f:
         connectivity = f['connectivity'][:]
         coordinates = f['coordinates'][:]
         n_vertices = connectivity.shape[1]
         vertices = np.reshape(coordinates[connectivity], newshape=(-1, n_vertices, 3))
-        domain_length_max = np.max(np.max(coordinates, axis=0) - np.min(coordinates, axis=0))
+        if domain_extent is None:
+            domain_extent = np.max(coordinates, axis=0) - np.min(coordinates, axis=0)
         centers = np.mean(vertices, axis=1)
         dtypes = [('position_x', 'f8'), ('position_y', 'f8'), ('position_z', 'f8')]
         if 'level' not in f.keys():
-            levels = np.round(-np.log2((vertices[:, 1, 0] - vertices[:, 0, 0]) / domain_length_max)).astype(int)
+            vertices_min = np.min(vertices, axis=1)
+            vertices_max = np.max(vertices, axis=1)
+            cell_extents = (vertices_max - vertices_min) / domain_extent
+            levels = np.round(-np.log2(cell_extents * coarse_bins)).astype(int)
             dtypes += [('level', 'i4')]
         else:
             levels = None
